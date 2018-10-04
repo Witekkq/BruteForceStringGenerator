975af96affb4c3a6f1648845304310f9346b12a5
witekkq <witekkq@gmail.com>
2018-10-04 13:55:26 +0100

    Added raising valueerror if maxlength

--- brute_force_string_generator.py
+++ brute_force_string_generator.py
@@ -3,10 +3,11 @@ import string
 
 class BruteForceStringGenerator(object):
 
-    def __init__(self, chars=string.ascii_lowercase, min_length=1, initial_sequence=[]):
+    def __init__(self, chars=string.ascii_lowercase, min_length=1, max_length=0, initial_sequence=[]):
         self.sequence = list(initial_sequence)
         self.chars = chars
         self.min_length = min_length
+	self.max_length = max_length
         self.number_of_chars = len(self.chars)
 
     def character_to_index(self, char):
@@ -27,6 +28,8 @@ class BruteForceStringGenerator(object):
             if not self.sequence:
                 return list(self.chars[0]*self.min_length)
             else:
+		if len(self.sequence) >= self.max_length:
+			raise ValueError("Max length")
                 return list(self.index_to_character(0))
         else:
             curr_string[0] = self.index_to_character((self.character_to_index(curr_string[0]) + 1) % self.number_of_chars)
