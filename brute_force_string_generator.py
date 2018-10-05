df1a64b15cb14a03dcbe6deb5763ab200984bbc6
Witekkq <witekkq@gmail.com>
2018-10-05 07:50:32 +0200

    Issue 2 maximum length (#4)
    * Added raising valueerror if maxlength
    
    * issue_1 raise exception on max_length

--- brute_force_string_generator.py
+++ brute_force_string_generator.py
@@ -3,20 +3,12 @@ import string
 
 class BruteForceStringGenerator(object):
 
-    def __init__(self, chars=string.ascii_lowercase, min_length=1, initial_sequence=[]):
+    def __init__(self, initial_sequence=[], chars=string.ascii_lowercase, min_length=1, max_length=0):
         self.sequence = list(initial_sequence)
         self.chars = chars
-        self.min_length = min_length
+        self.min_length = max(0, min_length)
+        self.max_length = max(0, max_length)
         self.number_of_chars = len(self.chars)
-
-    def character_to_index(self, char):
-        return self.chars.index(char)
-
-    def index_to_character(self, index):
-        if self.number_of_chars <= index:
-            raise ValueError("Index out of range.")
-        else:
-            return self.chars[index]
 
     def next_string(self):
         self.sequence = self._next(self.sequence)
@@ -27,9 +19,13 @@ class BruteForceStringGenerator(object):
             if not self.sequence:
                 return list(self.chars[0]*self.min_length)
             else:
-                return list(self.index_to_character(0))
+                if self.max_length and len(self.sequence) >= self.max_length:
+                    raise ValueError("Max length")
+                return list(self.chars[0])
         else:
-            curr_string[0] = self.index_to_character((self.character_to_index(curr_string[0]) + 1) % self.number_of_chars)
-            if self.character_to_index(curr_string[0]) is 0:
+            curr_string[0] = self.chars[((self.chars.index(curr_string[0]) + 1) % self.number_of_chars)]
+            if self.chars.index(curr_string[0]) is 0:
                 return list(curr_string[0]) + self._next(curr_string[1:])
         return curr_string
+
+
