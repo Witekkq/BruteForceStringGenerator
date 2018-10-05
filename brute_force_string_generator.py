db310a27ed807e396268059cc58084cccbd5abe6
Witold Kwinecki <witold.kwinecki@gmail.com>
2018-10-05 10:45:17 +0200

    Issue_1 direction available

--- brute_force_string_generator.py
+++ brute_force_string_generator.py
@@ -13,45 +13,32 @@ class Direction(Enum):
 
 class BruteForceStringGenerator(object):
 
-    def __init__(self, initial_sequence=[], chars=string.ascii_lowercase, direction=Direction.RIGHT, min_length=1, max_length=0):
-        self.sequence = list(initial_sequence)
+    def __init__(self, sequence=[], chars=string.ascii_lowercase, dir=Direction.RIGHT, min_length=1, max_length=0):
+        self.sequence = list(sequence)
         self.chars = chars
-        self.direction = int(direction)
+        self.dir = dir
         self.min_length = max(0, min_length)
         self.max_length = max(0, max_length)
-        self.number_of_chars = len(self.chars)
+        self.chars_num = len(self.chars)
 
     def next_string(self):
         self.sequence = self._next(self.sequence)
         return "".join(self.sequence)
 
-    def _next(self, curr_string):
-        if len(curr_string) <= 0:
+    def _next(self, current):
+        if len(current) <= 0:
             if not self.sequence:
-                return list(self.chars[0]*self.min_length)
+                return list(self.chars[0] * self.min_length)
             else:
                 if self.max_length and len(self.sequence) >= self.max_length:
                     raise ValueError("Max length")
                 return list(self.chars[0])
         else:
-            curr_string[self.direction] = self.chars[((self.chars.index(curr_string[self.direction]) + 1) % self.number_of_chars)]
-            if self.chars.index(curr_string[self.direction]) is 0:
-                print("curr_string {curr_string}".format(curr_string=curr_string))
-                print("curr_string[1:] {curr_string}".format(curr_string=curr_string[1:]))
-                if self.direction == Direction.LEFT:
-                    return list(curr_string[0]) + self._next(curr_string[1:])
+            current[int(self.dir)] = self.chars[((self.chars.index(current[int(self.dir)]) + 1) % self.chars_num)]
+            if self.chars.index(current[int(self.dir)]) == 0:
+                if self.dir == Direction.LEFT:
+                    return list(current[0]) + self._next(current[1:])
                 else:
-                    return list(curr_string[-1]) + self._next(curr_string[:-1])
-        return curr_string
+                    return self._next(current[:-1]) + list(current[-1])
+        return current
 
-brute_string = BruteForceStringGenerator(initial_sequence='zz', direction=Direction.LEFT)
-print(brute_string.next_string())
-
-brute_string = BruteForceStringGenerator(initial_sequence='zzz', direction=Direction.LEFT)
-print(brute_string.next_string())
-
-brute_string = BruteForceStringGenerator(initial_sequence='zz', direction=Direction.RIGHT)
-print(brute_string.next_string())
-
-brute_string = BruteForceStringGenerator(initial_sequence='zzz', direction=Direction.RIGHT)
-print(brute_string.next_string())
--- tests/test_brute_force_string_generator.py
+++ tests/test_brute_force_string_generator.py
@@ -10,50 +10,63 @@ def test_brute_force_empty():
 
 
 def test_brute_force_direction():
-    direction_test = BruteForceStringGenerator(initial_sequence='aa', direction=Direction.LEFT)
+    direction_test = BruteForceStringGenerator(sequence='aa', dir=Direction.LEFT)
     assert (direction_test.next_string() == 'ba')
-    direction_test = BruteForceStringGenerator(initial_sequence='aa', direction=Direction.RIGHT)
+    direction_test = BruteForceStringGenerator(sequence='aa', dir=Direction.RIGHT)
     assert (direction_test.next_string() == 'ab')
 
 
-def test_brute_long_string():
-    long_string = BruteForceStringGenerator(initial_sequence='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+def test_brute_force_edge():
+    direction_test = BruteForceStringGenerator(sequence='za', dir=Direction.LEFT)
+    assert (direction_test.next_string() == 'ab')
+    direction_test = BruteForceStringGenerator(sequence='az', dir=Direction.RIGHT)
+    assert (direction_test.next_string() == 'ba')
+
+
+def test_brute_force_long_string():
+    long_string = BruteForceStringGenerator(sequence='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     assert (long_string.next_string() == 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab')
 
 
-def test_brute_for_loop():
+def test_brute_force_for_loop():
     loop_string = BruteForceStringGenerator(min_length=3)
-    for i in range(1, len(string.ascii_lowercase)):
+    for _ in range(1, len(string.ascii_lowercase)):
         loop_string.next_string()
-    assert (loop_string.next_string() == 'zaa')
+    assert (loop_string.next_string() == 'aaz')
 
-    for i in range(1, len(string.ascii_lowercase)):
+    for _ in range(1, len(string.ascii_lowercase)):
         loop_string.next_string()
-    assert (loop_string.next_string() == 'zba')
+    assert (loop_string.next_string() == 'abz')
 
-    for i in range(1, len(string.ascii_lowercase*len(string.ascii_lowercase))):
+    for _ in range(1, len(string.ascii_lowercase * len(string.ascii_lowercase))):
         loop_string.next_string()
-    assert (loop_string.next_string() == 'zbb')
+    assert (loop_string.next_string() == 'bbz')
 
 
-def test_brute_custom_chars():
-
+def test_brute_force_custom_chars():
     custom_string = BruteForceStringGenerator(chars='!@#', min_length=3)
     assert (custom_string.next_string() == '!!!')
-    assert (custom_string.next_string() == '@!!')
-    assert (custom_string.next_string() == '#!!')
+    assert (custom_string.next_string() == '!!@')
+    assert (custom_string.next_string() == '!!#')
 
 
-def test_brute_brute():
-
-    brute_string = BruteForceStringGenerator()
+def test_brute_force_brute_left():
+    brute_string = BruteForceStringGenerator(dir=Direction.LEFT)
     for _ in range(1, 2000000):
         brute_string.next_string()
 
     assert (brute_string.next_string() == 'botid')
 
 
-def test_max_length():
+def test_brute_force_brute_right():
+    brute_string = BruteForceStringGenerator()
+    for _ in range(1, 2000000):
+        brute_string.next_string()
+
+    assert (brute_string.next_string() == 'ditob')
+
+
+def test_brute_force_max_length():
     with pytest.raises(ValueError):
         brute_string = BruteForceStringGenerator('z', max_length=1)
         brute_string.next_string()
